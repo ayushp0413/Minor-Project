7f568a7e7bec72d4b747856931e7a9f10c9c8bc2
Ayush Patidar <ayushpatidar7441@gmail.com>
2023-10-25 20:08:50 +0530

    images

--- order.js
+++ order.js
@@ -1,20 +1,20 @@
 
-const element = document.querySelector("body")
-let taskDone = false
+// const element = document.querySelector("body")
+// let taskDone = false
 
-// function for show button
-const elementShow = () => {
-  element.innerHTML = `
-    <button id="order-button" style="background-color : ${!taskDone 
-      ? "#4dd432" : "#fd67ad"};" onclick=doneTask()>
-      ${!taskDone ? "Done" : "not done yet"}
-    </button>
-    `
-}
-elementShow()
+// // function for show button
+// const elementShow = () => {
+//   element.innerHTML = `
+//     <button id="order-button" style="background-color : ${!taskDone 
+//       ? "#4dd432" : "#fd67ad"};" onclick=doneTask()>
+//       ${!taskDone ? "Done" : "not done yet"}
+//     </button>
+//     `
+// }
+// elementShow()
 
-// click Done button
-const doneTask = () => {
-  taskDone = (taskDone ? false : true)
-  elementShow()
-}
+// // click Done button
+// const doneTask = () => {
+//   taskDone = (taskDone ? false : true)
+//   elementShow()
+// }
--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 const product = [
    {
        id: 0,
-       image: 'image/gg-1.jpg',
-       title: 'Z Flip Foldable Mobile',
+       image: 'image/aa-1.jpg',
+       title: 'Veggie Pizza',
        price: 120,
    },
    {
        id: 1,
-       image: 'image/hh-2.jpg',
-       title: 'Air Pods Pro',
+       image: 'image/bb-1.jpg',
+       title: ' Burger Special',
        price: 60,
    },
    {
        id: 2,
-       image: 'image/ee-3.jpg',
-       title: '250D DSLR Camera',
+       image: 'image/cc-1.jpg',
+       title: 'Roasted Chicken ',
        price: 230,
    },
    {
        id: 3,
        image: 'image/aa-1.jpg',
-       title: 'Head Phones',
+       title: 'Pizza',
        price: 180,
    },
    {
     id: 4,
     image: 'image/aa-1.jpg',
-    title: 'Head Phones',
+    title: 'Pizza',
     price: 299,
     },
     {
         id: 5,
         image: 'image/aa-1.jpg',
-        title: 'Head Phones',
+        title: 'Pizza',
         price: 250,
     },
     {
@@ -95,15 +95,16 @@ const categories = [...new Set(product.map((item)=>
    let i=0;
 document.getElementById('root').innerHTML = categories.map((item)=>
 {
+
    var {image, title, price} = item;
    return(
-       `<div class='box'>
+       `<div class='box'>                
            <div class='img-box'>
                <img class='images' src=${image}></img>
            </div>
        <div class='bottom'>
        <p>${title}</p>
-       <h2>$ ${price}.00</h2>`+
+       <h2>${price}.00</h2>`+
        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
        `</div>
        </div>`
@@ -126,39 +127,40 @@ function displaycart(){
    document.getElementById("count").innerHTML=cart.length;
    if(cart.length==0){
        document.getElementById('cartItem').innerHTML = "Your cart is empty";
-       document.getElementById("total").innerHTML = "$ "+0+".00";
+       document.getElementById("total").innerHTML = " "+0+".00";
    }
    else{
        document.getElementById("cartItem").innerHTML = cart.map((items)=>
        {
            var {image, title, price} = items;
            total=total+price;
-           document.getElementById("total").innerHTML = "$ "+total+".00";
+           document.getElementById("total").innerHTML = " "+total+".00";
            return(
                `<div class='cart-item'>
                <div class='row-img'>
                    <img class='rowimg' src=${image}>
                </div>
                <p style='font-size:12px;'>${title}</p>
-               <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+               <h2 style='font-size: 15px;'>${price}.00</h2>`+
                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
            );
        }).join('');
    }   
 }
-
-
 document.addEventListener("click", (e) =>
  {
     // Complete Btn
-    if (e.target.id == "order-button") 
+    if(cart.length==0)
+    {
+        document.getElementById('cartItem').innerHTML = "Please add some item";
+
+    }
+    else if (e.target.id == "order-button" ) 
     {
       document.querySelector(".model").style.display = "block";
     }
 }
 )
-
-
 document.addEventListener("click", (e) =>
  {
     // Complete Btn
