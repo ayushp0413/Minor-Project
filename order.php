70503fe4c7f764ee42d7196717acb8e838b3c0e1
Ayush Patidar <ayushpatidar7441@gmail.com>
2023-11-05 09:48:17 +0530

    backend_changes

--- order.php
+++ order.php
@@ -1,31 +1,47 @@
-<?php
+ <?php
 
 if(isset($_POST['submit']))
 {
     $table = $_POST['table'];
     $name = $_POST['name'];
     $mobile = $_POST['mobile'];
-   
 
-    $conn = mysqli_connect('localhost','root','','food');
-
-   // if(table no already taken not allot )
-    //{
-
-    //}
-    //else{
+        $conn = mysqli_connect('localhost','root','','food');
 
         $sql = "INSERT INTO customers(tableno,name,mobile) values ('$table','$name','$mobile')";
         
-        mysqli_query($conn,$sql);
-        
-        echo "Thankyou! Your Order is Placed."; 
-    //}
+        $data = mysqli_query($conn,$sql);
     
+        ?>
+            <head>
+                <link rel="stylesheet" href="order_style.css">
+            </head>
+             <h1>THANKS! Your Order Is Placed.</h1>
+           
+             <div class="container">
+                <div class="item1">
+                    <button id="close"> <a href="index.html">Back to Menu</a></button>
+                </div>
+                <a href="index.html">
+                <div class="item2">
+                    <button  onclick="mycancel()" id="cancel-order">Cancel Order</button>
+                </div>
+                </a>
 
-
-
-
+                <div class="item3">
+                    <button id="view-order"><a href="view.php">View Order</a></button>
+                </div>
+             </div>
+              
+             <script>
+                    function mycancel(){
+                    alert("Your Order is Cancelled !!");
+                }
+                  
+            </script>
+      
+        <?php
+        
 
 }
 ?>
